99e1010a263d6efe51240dadd8b71ee501443aab
Joyce Tagbovi <info@businesslinksafrica.org>
2023-08-07 09:31:17 +0000

    next js test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 
 export default function Home() {
+  // const supabase = createServerComponentClient({cookies});
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
